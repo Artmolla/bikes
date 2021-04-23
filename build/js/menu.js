bd75feeb96c7b08098692f7c5a07f3791b51be5e
Artem <artmolla@gmail.com>
2021-04-23 19:09:56 +0300

    Добавляет файловую структуру для saas папки

--- build/js/menu.js
+++ build/js/menu.js
@@ -1,19 +1,19 @@
-// 'use strict';
+'use strict';
 
-// (() => {
-//   const menu = document.querySelector('.menu');
-//   const menuList = menu.querySelector('.menu__list');
-//   const menuToggle = document.querySelector('.menu__toggle');
+(() => {
+  const menu = document.querySelector('.menu');
+  const menuList = menu.querySelector('.menu__list');
+  const menuToggle = document.querySelector('.menu__toggle');
 
-//   menu.classList.remove('menu--no-js');
-//   menuList.classList.remove('menu__list--no-js');
-//   menuToggle.classList.remove('menu__toggle--no-js');
+  menu.classList.remove('menu--no-js');
+  menuList.classList.remove('menu__list--no-js');
+  menuToggle.classList.remove('menu__toggle--no-js');
 
-//   menuToggle.addEventListener('click', (evt) => {
-//     evt.preventDefault();
+  menuToggle.addEventListener('click', (evt) => {
+    evt.preventDefault();
 
-//     menuToggle.classList.toggle('menu__toggle--open');
-//     menu.classList.toggle('menu--open');
-//     menuList.classList.toggle('menu__list--open');
-//   });
-// })();
+    menuToggle.classList.toggle('menu__toggle--open');
+    menu.classList.toggle('menu--open');
+    menuList.classList.toggle('menu__list--open');
+  });
+})();
